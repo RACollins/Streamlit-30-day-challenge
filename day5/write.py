59f59e25612d20911106b436bda6b5e41c71e6f0
RACollins <richardcollins128@mail.com>
2023-11-10 23:38:43 +0900

    day8 commit

--- day5/write.py
+++ day5/write.py
@@ -17,8 +17,8 @@ st.write(1234)
 
 #df = pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
 df = pd.DataFrame(
-    index=["a", "b", "c", "d"],
-    columns=["A", "B", "C", "D"],
+    index=["a", "b", "c", "d", "e", "f", "g"],
+    columns=["A", "B", "C", "D", "E", "F", "G"],
     data=np.random.randint(0, 9, (4, 4)),
 )
 st.write(df)
